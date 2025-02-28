3dfe429ec0ba52b4928cf00b14dd2a34821394e9
Eman Naveed <zlac358@live.rhul.ac.uk>
2025-02-28 16:32:10 +0000

    JavaDoc for LSystem, Rule, and Turtle

--- src/Rule.java
+++ src/Rule.java
@@ -1,13 +1,37 @@
+/**
+ * Rule object class stores a letter with its corresponding generation rule.
+ *
+ * @author Eman Naveed
+ */
 public class Rule {
     char a;
     String b;
+
+    /**
+     * Constructor for class Rule
+     *
+     * @param a_ Letter
+     * @param b_ Rule corresponding to letter
+     */
     Rule(char a_, String b_){
         a = a_;
         b = b_;
     }
+
+    /**
+     * Getter for letter
+     *
+     * @return letter
+     */
     char getA(){
         return a;
     }
+
+    /**
+     * Getter for rule
+     *
+     * @return rule String
+     */
     String getB(){
         return b;
     }
--- src/Turtle.java
+++ src/Turtle.java
@@ -1,13 +1,37 @@
+/**
+ * Turtle object class that determines graphical output
+ *
+ * @author Eman Naveed
+ */
 public class Turtle {
     float length;
     float theta;
+
+    /**
+     * Constructor for class Turtle
+     *
+     * @param l Length of one drawn unit
+     * @param t Angle of rotation
+     */
     Turtle(float l, float t){
         length = l;
         theta = t;
     }
 
+    /**
+     * Creates graphical output using basic turtle commands
+     * <p>
+     *     Note: All references to Sketch.processing are required as processing cannot be accessed in this class except
+     *     through the PApplet.
+     * </p>
+     *
+     * @param sentence This determines the graphical output according to the l-system
+     */
     void render(String sentence){
+        // stroke( colour, opacity)
         Sketch.processing.stroke(0, 175);
+
+        // traverses through sentence and determines graphical actions corresponding to specific characters
         for(int i = 0; i < sentence.length(); i++){
             char c = sentence.charAt(i);
             switch (c){
@@ -17,15 +41,19 @@ public class Turtle {
                     Sketch.processing.translate(length, 0);
                     break;
                 case '+':
+                    // rotate right
                     Sketch.processing.rotate(theta);
                     break;
                 case '-':
+                    // rotate left
                     Sketch.processing.rotate(-theta);
                     break;
                 case '[':
+                    // saves branching off position
                     Sketch.processing.pushMatrix();
                     break;
                 case ']':
+                    // returns to branching off position to continue drawing from there
                     Sketch.processing.popMatrix();
             }
 
