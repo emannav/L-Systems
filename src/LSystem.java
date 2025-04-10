42069586e73897de6df812a343692d13e6fdeb3d
Eman Naveed <zlac358@live.rhul.ac.uk>
2025-04-10 18:51:46 +0100

    Stochastic implementation functional; window setup needs refining

--- src/LSystem.java
+++ src/LSystem.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * LSystem object replicates strings as determined by the corresponding ruleset.
@@ -9,20 +11,24 @@ import java.util.List;
 public class LSystem {
     String sentence;
     List<Rule> ruleset;
+    Boolean stochastic;
 
     /**
-     * Constructor for class LSystem
+     * Constructor for class LSystem.
      *
-     * @param axiom Input string to be replicated
-     * @param rules Rule set dictating how replication will occur
+     * @param axiom Input string to be replicated.
+     * @param rules Rule set dictating how replication will occur.
+     * @param stochastic Determines which generator function will be used.
      */
-    LSystem(String axiom, List<Rule> rules){
-        this.sentence = valid(axiom);
+    LSystem(String axiom, List<Rule> rules, Boolean stochastic){
+        this.sentence = axiom;
         this.ruleset = rules ;
+        this.stochastic = stochastic;
     }
 
     /**
-     * Fetches String corresponding to letters in the ruleset to generate a new replicated String
+     * Builds latest production using the existing ruleset
+     *
      * <p>
      *     Note: Instead of returning the string to the Turtle object, generate changes the value of the local sentence
      *     variable. The Turtle then accesses this String using the getter. This confines all String related functions
@@ -32,40 +38,41 @@ public class LSystem {
     public void generate(){
         StringBuilder nextgen = new StringBuilder();
         char c;
-        for(int i = 0; i < sentence.length(); i++){
-            // variable c temporarily stores letters
-            c = sentence.charAt(i);
-            // checks if letter has generation rule corresponding to it
-            if(contains(c)) {
-                // if True, append generation to StringBuilder
-                nextgen.append(Generator(c));
+        if (!stochastic){
+            for(int i = 0; i < sentence.length(); i++){
+
+                // variable c temporarily stores letters
+                c = sentence.charAt(i);
+
+                // checks if letter has generation rule corresponding to it
+                if(contains(c)) {
+                    // if True, append generation to StringBuilder
+                    nextgen.append(generator(c));
+                }
+                else{
+                    // if False, append letter unchanged
+                    nextgen.append(c);
+                }
             }
-            else{
-                // if False, append letter unchanged
-                nextgen.append(c);
+        }
+        else {
+            for(int i = 0; i < sentence.length(); i++){
+
+                // variable c temporarily stores letters
+                c = sentence.charAt(i);
+
+                // checks if letter has generation rule corresponding to it
+                if(contains(c)) {
+                    // if True, append generation to StringBuilder
+                    nextgen.append(stochGenerator(c));
+                }
+                else{
+                    // if False, append letter unchanged
+                    nextgen.append(c);
+                }
             }
         }
         this.sentence = nextgen.toString();
-    }
-
-    /**
-     * Removes any unrecognized characters in the input String
-     *
-     * @param begin Input string to be generated
-     * @return Final sentence with unrecognized characters removed
-     */
-    public String valid(String begin){
-        char comp;
-        StringBuilder finalStr = new StringBuilder();
-        // loops traverse through input string
-        for(int i = 0; i < begin.length(); i++){
-            comp = begin.charAt(i);
-            // checks validity of characters in String
-            if(comp == 'F' || comp == 'G' || comp == '+' || comp == '-' || comp == '[' || comp == ']'){
-                finalStr.append(comp);
-            }
-        }
-        return finalStr.toString();
     }
 
     /**
@@ -87,8 +94,9 @@ public class LSystem {
         char comp;
         // traverses through rule set
         for (Rule rule : ruleset){
-            // checks if character is the same as rule
             comp = rule.getA();
+
+            // checks if character has rule corresponding to it
             if (c == comp){
                 return true;
             }
@@ -102,15 +110,61 @@ public class LSystem {
      * @param c Letter to get the rule for
      * @return Rule corresponding to input letter
      */
-    public String Generator(char c){
+    public String generator(char c){
+        // temporary comparison character
         char comp;
 
+        // iterates through the stored ruleset
         for (Rule rule : ruleset){
             comp = rule.getA();
+
+            // returns rule corresponding to character
             if (c == comp){
                 return rule.getB();
             }
         }
         return null;
     }
+
+    /**
+     * Fetches generation String according to probability of outcome
+     *
+     * @param c Letter to get the rule for
+     * @return Rule corresponding to input letter
+     */
+    public String stochGenerator(char c){
+        // temporary comparison character
+        char comp;
+        Random random = new Random();
+
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<Double> probabilities = new ArrayList<>();
+
+        Double cumulativeProbability = 0.0;
+        // random number used to determine outcome
+        double randomVal = random.nextDouble();
+
+        for (Rule rule : ruleset){
+            comp = rule.getA();
+            // compiles all strings and their probabilities into specific ArrayLists
+            if (c == comp){
+                strings.add(rule.getB());
+                probabilities.add(rule.getC());
+            }
+        }
+
+        // cutoff values defined based on cumulative probabilities
+        // ensures that each string has a predetermined chance of being selected
+        for (int i = 0; i < strings.size(); i++) {
+            cumulativeProbability += probabilities.get(i);
+
+            // outcome selected
+            if (randomVal <= cumulativeProbability) {
+                return strings.get(i);
+            }
+        }
+        // in case of rounding issues
+        return strings.getLast();
+    }
+
 }
--- src/Sketch.java
+++ src/Sketch.java
@@ -12,8 +12,10 @@ public class Sketch extends PApplet {
     LSystem kochSnowflake;
     Turtle turtleST;
     LSystem serpTriangle;
-    Turtle turtleSL;
-    LSystem stochasticImpl;
+    Turtle turtleTree;
+    LSystem lTree;
+    Turtle turtleStoch;
+    LSystem basicStoch;
 
     public void settings(){
         size(1500, 800);
@@ -26,13 +28,13 @@ public class Sketch extends PApplet {
         Rule ruleKC = new Rule('F', "F+F-F-F+F");
         List<Rule> rulesKC = new ArrayList<Rule>();
         rulesKC.add(ruleKC);
-        kochCurve = new LSystem("F", rulesKC);
+        kochCurve = new LSystem("F", rulesKC, false);
 
         this.turtleKS = new Turtle(5, radians(60));
         Rule ruleKS = new Rule('F', "F-F++F-F");
         List<Rule> rulesKS = new ArrayList<Rule>();
         rulesKS.add(ruleKS);
-        kochSnowflake = new LSystem("F++F++F", rulesKS);
+        kochSnowflake = new LSystem("F++F++F", rulesKS, false);
 
         this.turtleST = new Turtle(6, radians(60));
         List<Rule> rulesLC = new ArrayList<>();
@@ -40,46 +42,64 @@ public class Sketch extends PApplet {
         rulesLC.add(ruleLC);
         ruleLC = new Rule('G', "F+G+F");
         rulesLC.add(ruleLC);
-        serpTriangle = new LSystem("F+G+F-G-F-G-F+G+F", rulesLC);
+        serpTriangle = new LSystem("F+G+F-G-F-G-F+G+F", rulesLC, false);
 
-        this.turtleSL = new Turtle(6, radians(25));
-        List<Rule> rulesSL = new ArrayList<>();
-        Rule ruleSL = new Rule('F', "FF+[+F-F-F]-[-F+F+F]");
-        rulesSL.add(ruleSL);
-        stochasticImpl = new LSystem("F", rulesSL);
+        this.turtleTree = new Turtle(6, radians(25));
+        List<Rule> rulesTree = new ArrayList<>();
+        Rule ruleT = new Rule('F', "FF+[+F-F-F]-[-F+F+F]");
+        rulesTree.add(ruleT);
+        lTree = new LSystem("F", rulesTree, false);
+
+        this.turtleStoch = new Turtle(8, radians(30));
+        List<Rule> rulesBS = new ArrayList<>();
+        Rule ruleBS1 = new Rule('F', "F[+F]F[-F]F", 0.33);
+        Rule ruleBS2 = new Rule('F', "F[+F]F", 0.33);
+        Rule ruleBS3 = new Rule('F', "F[-F]F", 0.34);
+        rulesBS.add(ruleBS1);
+        rulesBS.add(ruleBS2);
+        rulesBS.add(ruleBS3);
+        basicStoch = new LSystem("F", rulesBS, true);
     }
 
     public void draw() {
         background(255);
-        translate(0,5);
-        for(int i = 0; i < 4; i++) {
-            turtleKC.render(kochCurve());
-            translate(5, 0);
-        }
-
-        translate(-500, 300);
-
-        for(int i = 0; i < 3; i++) {
-            turtleKS.render(kochSnowflake());
-            translate(80, 0);
-        }
-        translate(-20, 90);
-        turtleKS.render(kochSnowflake());
-
-        translate(-50,650);
-        for(int i = 0; i < 4; i++) {
-            translate(0, -70);
-            turtleST.render(serpTriangle());
-        }
-
+        translate(200, 600);
         rotate(-90);
-        translate(150, -200);
-        for(int i = 0; i < 3; i++){
-            turtleSL.render(stochasticImpl());
-            translate(-60, 90);
+
+        for (int i = 0; i < 4; i++){
+            basicStoch.generate();
+            turtleStoch.render(basicStoch.getSentence());
+            rotate(radians(turtleStoch.getTheta() * 3));
+            translate(-(i * 100), (i * 80));
         }
-        translate(-70, 70);
-        turtleSL.render(stochasticImpl());
+//        for(int i = 0; i < 4; i++) {
+//            turtleKC.render(kochCurve());
+//            translate(5, 0);
+//        }
+//
+//        translate(-500, 300);
+//
+//        for(int i = 0; i < 3; i++) {
+//            turtleKS.render(kochSnowflake());
+//            translate(80, 0);
+//        }
+//        translate(-20, 90);
+//        turtleKS.render(kochSnowflake());
+//
+//        translate(-50,650);
+//        for(int i = 0; i < 4; i++) {
+//            translate(0, -70);
+//            turtleST.render(serpTriangle());
+//        }
+//
+//        rotate(-90);
+//        translate(150, -200);
+//        for(int i = 0; i < 3; i++){
+//            turtleTree.render(stochasticImpl());
+//            translate(-60, 90);
+//        }
+//        translate(-70, 70);
+//        turtleTree.render(stochasticImpl());
 
         noLoop();
     }
@@ -87,6 +107,7 @@ public class Sketch extends PApplet {
     public static void main(String[] args){
         PApplet.main("Sketch", args);
     }
+
 
     public String kochCurve(){
         kochCurve.generate();
@@ -104,7 +125,7 @@ public class Sketch extends PApplet {
     }
 
     public String stochasticImpl(){
-        stochasticImpl.generate();
-        return stochasticImpl.getSentence();
+        lTree.generate();
+        return lTree.getSentence();
     }
 }
